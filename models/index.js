d5c2d5cae8e5f01ea019a31865b79ab41c394c7e
ibraim martinez <ibraimm5@gmail.com>
2021-08-29 23:50:42 -0400

    added association methods

--- models/index.js
+++ models/index.js
@@ -5,12 +5,22 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.hasOne(Category, {
+  foreignKey: "product_id",
+  onDelete: "CASCADE",
+});
 // Categories have many Products
-
+Category.hasMany(Product, {
+  foreignKey: "product_id"
+});
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(ProductTag, {
+  foreignKey: "product_id"
+}); 
 // Tags belongToMany Products (through ProductTag)
+ProductTag.belongsToMany(Product, {
+  foreignKey: "product_id"
+}); 
 
 module.exports = {
   Product,
